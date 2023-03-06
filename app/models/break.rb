a1ad9b8adeff784688a41ef2329a778caf95f630
tansioooooooo <yuhei.tanno.922@gmail.com>
2023-03-06 17:00:31 +0900

    add work relation to break

--- app/models/break.rb
+++ app/models/break.rb
@@ -2,6 +2,7 @@ class Break
   include Mongoid::Document
   include Mongoid::Timestamps
   field :user_id, type: Integer
+  field :work_id, type: Integer
   field :date, type: Date
   field :start_time, type: Time
   field :end_time, type: Time
