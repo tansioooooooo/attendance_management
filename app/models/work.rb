8bbc7700527a3bdef04d281e3dd732b970961aa2
tansioooooooo <yuhei.tanno.922@gmail.com>
2023-03-06 16:52:29 +0900

    add works model

---
+++ app/models/work.rb
@@ -0,0 +1,9 @@
+class Work
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :user_id, type: Integer
+  field :date, type: Date
+  field :start_time, type: Time
+  field :end_time, type: Time
+  field :abcent, type: Mongoid::Boolean
+end
---
+++ app/views/works/_work.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! work, :id, :user_id, :date, :start_time, :end_time, :abcent, :created_at, :updated_at
+json.url work_url(work, format: :json)
